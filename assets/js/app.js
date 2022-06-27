b0976edfcfdd26999c08c90a1e082040155fd3d0
Renaldo Freire <renaldo@disroot.org>
2022-06-27 00:19:12 -0300

    finish social and footer sections

---
+++ assets/js/app.js
@@ -0,0 +1,11 @@
+
+const scrollUp = document.querySelector("#scroll-up");
+
+scroll.Up.addEventListener("click", () => {
+  window.scroll({
+    top: 0,
+    left: 0,
+    behavior: "smooth",
+  })
+
+})
